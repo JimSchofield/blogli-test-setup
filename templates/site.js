b5196a4c6211c61fe147ac076a004a557f2afd94
JimSchofield <oldcoyote@gmail.com>
2020-08-23 21:06:56 -0500

    Iteration- assets copying, templates working

--- templates/site.js
+++ templates/site.js
@@ -7,7 +7,9 @@ module.exports = ({siteTitle, assetsDir}) => ({ meta, content}) => (`
 <head>
     <meta charset="UTF-8">
     <title>${siteTitle + " - " + meta.title}</title>
+    <link href="/${assetsDir}/test.css" rel="stylesheet" type="text/css"/>
     <link href="/${assetsDir}/prism.css" rel="stylesheet" type="text/css"/>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet"> 
     <style>body { margin: 0; }</style>
 </head>
 <body>
